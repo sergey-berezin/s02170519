7045fca5214177cf4e4d7f75bfa8110ff35c3a4f
Deesice <gordey1703@yandex.ru>
2020-10-02 14:09:37 +0300

    Initial commit

---
+++ Eremin2020/Eremin2020/Program.cs
@@ -0,0 +1,38 @@
+﻿using MyLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Eremin2020
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var c = new CancellationTokenSource();
+            var net = new ResNet();
+            Console.WriteLine("Press the ENTER key to cancel...");
+            Task cancelTask = Task.Run(() =>
+            {
+                while (Console.ReadKey().Key != ConsoleKey.Enter)
+                {
+                    Console.WriteLine("Press the ENTER key to cancel...");
+                }
+                Console.WriteLine("\nENTER key pressed: cancelling downloads.\n");
+                c.Cancel();
+            });
+            var tasks = net.ProcessDirectory("pictures", c.Token);
+            try
+            {
+                await Task.WhenAll(tasks);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ Eremin2020/Eremin2020/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Eremin2020")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Eremin2020")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9a685e0a-b37a-49ce-a810-c52da398608b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
